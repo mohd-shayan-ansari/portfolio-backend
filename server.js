3257286c38023d953be3c1467a0f13f324576e64
mohd-shayan-ansari <mohdshayan0010@gmail.com>
2025-12-30 23:29:10 +0530

    Allow all CORS for testing

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['https://sparkly-crumble-f3699b.netlify.app', 'http://localhost:3000'],
+     origin: true,
     credentials: true
 }));
 app.use(express.json());
