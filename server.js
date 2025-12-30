5458048a6a7d53063274e0a1a12576583951b960
mohd-shayan-ansari <mohdshayan0010@gmail.com>
2025-12-30 21:57:23 +0530

    Fix CORS for Netlify

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-origin: '*', // Change this to your frontend URL
+    origin: ['https://sparkly-crumble-f3699b.netlify.app', 'http://localhost:3000'],
     credentials: true
 }));
 app.use(express.json());
